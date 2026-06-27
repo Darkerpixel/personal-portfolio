7e003a6a3d669067761302eaaf021dfeb3983622
Swetlana Warkentin <smam@mail.de>
2026-06-27 15:12:23 +0200

    old-form-back

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 //main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/styles.css'
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
