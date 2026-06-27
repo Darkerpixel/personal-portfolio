f73bbfe45b4eb4ca1f801af0d2cebc493eb044b7
Darkerpixel <warkentinant@gmail.com>
2026-06-27 17:12:51 +0200

    Typescript didn't recognize frontmatter

--- src/mdx.d.ts
+++ src/mdx.d.ts
@@ -1,5 +1,7 @@
 // src/mdx.d.ts
 declare module "*.mdx" {
+  export const frontmatter: Record<string, unknown>;
+
   const MDXComponent: (props: Record<string, unknown>) => JSX.Element;
   export default MDXComponent;
 }
