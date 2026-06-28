3dc6d09b48b178bc63075b80bb72e278dec09775
Darkerpixel <warkentinant@gmail.com>
2026-06-28 21:56:46 +0200

    implemented working unstyled terms
    including some test data inside the English .mdx file

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
 export type Language = "en" | "de";
 export type ImageProps = { src: string; alt: string };
 export type ImagesProps = { images: ImageProps[] };
+export type ReferenceProps = { reference: string; referenceText: string };
